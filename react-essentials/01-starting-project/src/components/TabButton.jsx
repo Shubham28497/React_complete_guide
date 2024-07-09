718c5900a4738d31998a7429ef3c0cc28d9cba94
shubham gupta <shubhamgupta24640020@gmail.com>
2024-07-09 16:33:16 +0530

    pass props as func

--- react-essentials/01-starting-project/src/components/TabButton.jsx
+++ react-essentials/01-starting-project/src/components/TabButton.jsx
@@ -1,5 +1,9 @@
-export default function TabButton(props){
-    return(
-        <li><button>{props.children}</button></li>
-    )
+export default function TabButton({ label,onSelect }) {
+ 
+
+  return (
+    <li>
+      <button onClick={onSelect}>{label}</button>
+    </li>
+  );
 }
