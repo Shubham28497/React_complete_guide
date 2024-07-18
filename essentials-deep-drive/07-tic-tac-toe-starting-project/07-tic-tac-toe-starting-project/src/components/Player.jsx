31c583eed4914dc2cefc5343eed2ff25de47a8bc
shubham gupta <shubhamgupta24640020@gmail.com>
2024-07-18 09:11:04 +0530

    onclick btn

--- essentials-deep-drive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ essentials-deep-drive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,23 @@
-export default function Player({name,symbol}){
-    return(
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setEditing] = useState(false);
+
+  function handleEditClick() {
+    setEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) {
+    playerName = <input type="text" required />;
+  }
+  return (
     <li>
-    <span className="player">
-      <span className="player-name">{name}</span>
-      <span className="player-symbol">{symbol}</span>
-    </span>
-    <button>Edit</button>
-  </li>
-    )
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>Edit</button>
+    </li>
+  );
 }
