e0d4b9c20b2102b3913335f36edcd89b44549033
shubham gupta <shubhamgupta24640020@gmail.com>
2024-07-25 10:10:36 +0530

    updated comp

--- essentials-deep-drive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ essentials-deep-drive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -7,13 +7,17 @@ export default function Player({ initialName, symbol }) {
   function handleEditClick() {
     setEditing((editing)=>!editing);
   }
-
+  function handleChange(event){
+    console.log(event)
+    setPlayerName(event.target.value)
+  }
   let editablePlayerName = <span className="player-name">{playerName}</span>;
 //   let btnCaption = "Edit";
   if (isEditing) {
-    editablePlayerName = <input type="text" required value={playerName} />;
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
     // btnCaption = "Save";
   }
+ 
   return (
     <li>
       <span className="player">
