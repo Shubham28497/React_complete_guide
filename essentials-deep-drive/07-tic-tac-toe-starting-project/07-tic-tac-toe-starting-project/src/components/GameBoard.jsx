970e954540aca14a6b36b702733c902b15fb19ac
shubham gupta <shubhamgupta24640020@gmail.com>
2024-07-25 11:50:34 +0530

    derving state from props

--- essentials-deep-drive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ essentials-deep-drive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -5,18 +5,24 @@ const intialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare,activePlayerSymbol}) {
-  const [gameBoard, setGameBoard] = useState(intialGameBoard);
-  function handleSelecSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameboard) => {
-      const updatedBoard = [
-        ...prevGameboard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
-    onSelectSquare()
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = intialGameBoard;
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
   }
+  //   const [gameBoard, setGameBoard] = useState(intialGameBoard);
+  //   function handleSelecSquare(rowIndex, colIndex) {
+  //     setGameBoard((prevGameboard) => {
+  //       const updatedBoard = [
+  //         ...prevGameboard.map((innerArray) => [...innerArray]),
+  //       ];
+  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //       return updatedBoard;
+  //     });
+  //     onSelectSquare()
+  //   }
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -24,9 +30,7 @@ export default function GameBoard({onSelectSquare,activePlayerSymbol}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelecSquare(rowIndex, colIndex)}>
-                  {playerSymbol}
-                </button>
+                <button onClick={()=>onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
