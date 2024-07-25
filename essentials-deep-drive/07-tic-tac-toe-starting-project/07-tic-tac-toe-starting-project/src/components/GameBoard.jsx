46422426405e524c2c3a9c149f676118ea721859
shubham gupta <shubhamgupta24640020@gmail.com>
2024-07-25 11:09:11 +0530

    lift up  state

--- essentials-deep-drive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ essentials-deep-drive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -5,16 +5,17 @@ const intialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare,activePlayerSymbol}) {
   const [gameBoard, setGameBoard] = useState(intialGameBoard);
   function handleSelecSquare(rowIndex, colIndex) {
     setGameBoard((prevGameboard) => {
       const updatedBoard = [
         ...prevGameboard.map((innerArray) => [...innerArray]),
       ];
-      updatedBoard[rowIndex][colIndex] = "X";
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
+    onSelectSquare()
   }
   return (
     <ol id="game-board">
