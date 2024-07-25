2b26a4fdd3a8ed44775eec4a3b56a471cea11802
shubham gupta <shubhamgupta24640020@gmail.com>
2024-07-25 09:19:56 +0530

    make a state of update the player name

--- essentials-deep-drive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ essentials-deep-drive/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName,setPlayerName]=useState(initialName)
   const [isEditing, setEditing] = useState(false);
 
   function handleEditClick() {
     setEditing((editing)=>!editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 //   let btnCaption = "Edit";
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = <input type="text" required value={playerName} />;
     // btnCaption = "Save";
   }
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing?"Save":"Edit"}</button>
